183c006b5b36c1a61fa914883e5677991bfe5d84
gael <gael.renault@epsi.fr>
2017-04-05 09:22:04 +0200

    Add activity + classe java Liste Patient

---
+++ app/src/main/java/com/epsi/takecare/ListePatient.java
@@ -0,0 +1,16 @@
+package com.epsi.takecare;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by gael on 05/04/2017.
+ */
+
+public class ListePatient extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_patients);
+    }
+}
